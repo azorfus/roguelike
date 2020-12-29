fc47a162448e753ec4cd15c59b93a62d0e9a0880
azorfus <72391760+azorfus@users.noreply.github.com>
2020-12-29 15:58:40 +0530

    Delete map.h

--- map.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAP_H
-#define MAP_H
-
-//void mapRem(int (&map)[6][10]);
-//int spawn(int (&map)[10][10], int x, int y, int p);
-void mapGen(int (&map)[10][10]);
-int printMAP(int map[10][10]);
-
-#endif
